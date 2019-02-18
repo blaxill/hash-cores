c5f7df6a98e79f0137350ebc4612c8f25b5ba9b0
Ben Blaxill <ben.blaxill@gmail.com>
2019-02-17 22:00:28 -0500

    Add iterable tester

--- src/Clash/HashCores/Class/Iterable.hs
+++ src/Clash/HashCores/Class/Iterable.hs
@@ -1,9 +1,20 @@
 {-# LANGUAGE FunctionalDependencies #-}
 
-module Clash.HashCores.Class.Iterable
+module Clash.HashCores.Class.Iterable (
+  Iterable,
+  preIteration,
+  postIteration,
+  oneStep,
+  iterableTester
+  )
   where
 
 import           Clash.Prelude
+
+-- For iterableTester
+import qualified Prelude as P
+import qualified Clash.Signal.Delayed.Bundle as B
+import Control.DeepSeq (NFData)
 
 class ( KnownNat rounds
       , KnownNat delay
@@ -23,3 +34,35 @@ class ( KnownNat rounds
              -- ^ Some input signal
           -> DSignal domain (reference+delay) s
              -- ^ Some output signal
+
+-- | Non-synthesizable function for basic testing
+--
+-- e.g.
+--
+-- >>> import Clash.HashCores.TH
+-- >>> import Clash.HashCores.Hash.SHA256
+-- >>> import Numeric (showHex)
+-- >>> lazyDog = $(asciiLit "The quick brown fox jumps over the lazy dog")
+-- >>> lazyDog' = preprocessI $ concatBitVector# lazyDog
+-- >>> hashed = iterableTester (SHA256 @0 @1) lazyDog'
+-- >>> showHex hashed ""
+-- "d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592"
+--
+iterableTester
+  :: forall x i s o r d.
+  ( Iterable x i s o r d
+  , NFData s
+  , KnownNat (r-1)
+  , 1 ~ d)
+  => x
+  -> i
+  -> o
+iterableTester x = postIteration x . go 0 . preIteration  x
+  where
+    f ix v = P.head . P.drop 1 . simulate (toSignal . uncurry (oneStep x) . B.unbundle . unsafeFromSignal) $ P.repeat (ix,v)
+    rounds = snatToNum (SNat @(r-1)) :: Index (r)
+
+    go n
+      | n == rounds = f n
+      | n /= rounds = go (n+1) . f n
+      | otherwise = id
