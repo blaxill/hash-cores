3130421c626b754aa3435196a6dcbb9f19a2b506
Ben Blaxill <ben.blaxill@gmail.com>
2019-03-04 18:07:29 -1000

    Update examples

--- examples/TopEntities.hs
+++ examples/TopEntities.hs
@@ -15,18 +15,54 @@
 --
 -- :verilog
 
+import           Clash.Prelude
+
+import           Clash.HashCores.Class.Circuit
 import           Clash.HashCores.Compositions
-import           Clash.HashCores.Core
+import           Clash.HashCores.Cores
 import           Clash.HashCores.Hash.SHA256
 
-import           Clash.Prelude
+
+systemClockSHAInPlace
+  :: Clock System 'Source
+  -> Reset System 'Asynchronous
+  -> DSignal System 0 (BitVector 512, Bool)
+  -> ( DSignal System 192 (BitVector 256)
+     , DSignal System 0 Bool)
+systemClockSHAInPlace =
+  exposeClockReset $ mkCircuit $ SimpleCore InPlace (SHA256 @1 @1)
+
+{-# ANN systemClockSHAInPlace
+  (Synthesize
+    { t_name   = "shaInPlace"
+    , t_inputs = [ PortName "clk"
+                 , PortName "rst"
+                 , PortProduct ""
+                   [ PortName "block"
+                   , PortName "valid"
+                   ]
+                 ]
+    , t_output = PortProduct ""
+                   [ PortName "hash"
+                   , PortName "ready"
+                   ]
+    }) #-}
+{-# NOINLINE systemClockSHAInPlace #-}
+-- SHA256 @0 @1
+-- LUTs  regs  slices
+-- 671   621   232
+--
+-- SHA256 @1 @1
+-- LUTs  regs  slices
 
 systemClockSHAPipelined
   :: Clock System 'Source
   -> Reset System 'Asynchronous
   -> DSignal System 0 (BitVector 512)
-  -> DSignal System 192 (BitVector 256)
-systemClockSHAPipelined = exposeClockReset (singleBlockPipe (Pipelined :. SHA256 @1 @1))
+  -> DSignal System 64 (BitVector 256)
+systemClockSHAPipelined =
+  exposeClockReset $ mkCircuitDataOnly $ SimpleCore Pipelined (SHA256 @0 @1)
+
 {-# ANN systemClockSHAPipelined
   (Synthesize
     { t_name   = "shaPipelined"
@@ -37,3 +73,13 @@ systemClockSHAPipelined = exposeClockReset (singleBlockPipe (Pipelined :. SHA256
     , t_output = PortName "hash"
     }) #-}
 {-# NOINLINE systemClockSHAPipelined #-}
+
+-- SHA256 @1 @1
+-- LUTs  regs  slices
+-- 23994 55565 9143
+-- 1.328 W
+--
+-- SHA256 @0 @1
+-- LUTs  regs  slices
+-- 25418 22426 6780
+-- 1.487 W
